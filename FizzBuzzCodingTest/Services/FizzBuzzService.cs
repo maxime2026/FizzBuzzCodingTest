dd23b45a8c0cb99191ab2f3413ff0063d6b69bf7
maxime fleury <fleury.maxime@hotmail.fr>
2023-02-15 21:44:54 +0000

    add main logic to check conditions

--- FizzBuzzCodingTest/Services/FizzBuzzService.cs
+++ FizzBuzzCodingTest/Services/FizzBuzzService.cs
@@ -1,4 +1,6 @@
-﻿using FizzBuzzCodingTest.Models;
+﻿using FizzBuzzCodingTest.GameConditions;
+using FizzBuzzCodingTest.GameConditions.Interfaces;
+using FizzBuzzCodingTest.Models;
 using FizzBuzzCodingTest.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -10,14 +12,43 @@ namespace FizzBuzzCodingTest.Services
 {
     public class FizzBuzzService : IFizzBuzzService
     {
+        private List<IGameCondition> _gameConditions;
+        private const int _minRange = 1;
+
         public FizzBuzzService()
         {
-
+            _gameConditions = new List<IGameCondition> 
+            {
+                new FizzBuzzCondition(),
+                new FizzCondition(),
+                new BuzzCondition()
+            };
         }
 
         public FizzBuzzResult GetResult(int range)
         {
-            throw new NotImplementedException();
+            if (range is <0 or >100)
+            {
+                return new FizzBuzzResult { SuccessFull = false, Error_message = "Invalid range" };
+            }
+
+            List<string> resultGameCondition = Enumerable.Range(_minRange, range)
+                .Select(number => {
+                    foreach (var condition in _gameConditions)
+                    {
+                        if (condition.IsValid(number))
+                        {
+                            return condition.Display();
+                        }
+                    }
+
+                    return number.ToString();
+                }).ToList();
+
+            return new FizzBuzzResult { 
+                Result = resultGameCondition,
+                SuccessFull = true
+            };
         }
     }
 }
