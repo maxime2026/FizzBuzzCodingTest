6ce231ccc3b61caf1915720f60dcf1f57385c76a
maxime fleury <fleury.maxime@hotmail.fr>
2023-02-15 21:13:46 +0000

    Add basic configuration on log and service, Add UI implementation

--- FizzBuzzCodingTest/Program.cs
+++ FizzBuzzCodingTest/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzzCodingTest.Services;
+using FizzBuzzCodingTest.Services.Interfaces;
+using FizzBuzzCodingTest.UI;
+using FizzBuzzCodingTest.UI.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var services = new ServiceCollection();
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+
+IConfiguration configuration = builder.Build();
+
+services.AddLogging(log =>
+{
+    log.AddConfiguration(configuration.GetSection("Logging"));
+    log.AddConsole();
+});
+
+services.AddTransient<IFizzBuzzService, FizzBuzzService>();
+services.AddTransient<IConsoleUI, ConsoleUI>();
+
+services.BuildServiceProvider()
+    .GetService<IConsoleUI>()?
+    .Run(args);
--- FizzBuzzCodingTest/UI/ConsoleUI.cs
+++ FizzBuzzCodingTest/UI/ConsoleUI.cs
@@ -1,4 +1,6 @@
-﻿using FizzBuzzCodingTest.UI.Interfaces;
+﻿using FizzBuzzCodingTest.Models;
+using FizzBuzzCodingTest.Services.Interfaces;
+using FizzBuzzCodingTest.UI.Interfaces;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,12 @@ namespace FizzBuzzCodingTest.UI
     public class ConsoleUI : IConsoleUI
     {
         private readonly ILogger _logger;
+        private readonly IFizzBuzzService _fizzBuzzService;
 
-        public ConsoleUI(ILogger<ConsoleUI> logger)
+        public ConsoleUI(IFizzBuzzService fizzBuzzService, ILogger<ConsoleUI> logger)
         {
             _logger = logger;
+            _fizzBuzzService = fizzBuzzService;
         }
 
         /// <summary>
@@ -24,7 +28,31 @@ namespace FizzBuzzCodingTest.UI
         /// <exception cref="ArgumentNullException"></exception>
         public void Run(string[] args)
         {
-            throw new NotImplementedException();
+            int range;
+            if (args == null || args.Count() == 0 || int.TryParse(args[0], out range) == false)
+            {
+                _logger.LogError("Value missing");
+                throw new ArgumentNullException("Value missing");
+            }
+
+            try
+            {
+                FizzBuzzResult fizzBuzzResult = _fizzBuzzService.GetResult(range);
+
+                if (fizzBuzzResult.SuccessFull)
+                {
+                    Console.WriteLine(string.Join(",", fizzBuzzResult.Result));                    
+                }
+                else
+                {
+                    Console.WriteLine(fizzBuzzResult.Error_message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("Internal application error", ex);
+                throw;
+            }
         }
     }
 }
